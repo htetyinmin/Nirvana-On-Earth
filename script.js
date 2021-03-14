2e7ab87bf1425485a763fc12f9f556f268045208
KyawWinTun-vix <vixenhello1998@gmail.com>
2021-03-14 16:35:24 +0630

    travel highlight

--- script.js
+++ script.js
@@ -31,6 +31,42 @@ $(document).ready(function () {
               enabled:true
         }
     })
+
+    //playhightlight
+    $('.video_link').magnificPopup({
+        type:'iframe',
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">'+
+                      '<div class="mfp-close"></div>'+
+                      '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                    '</div>',
+          
+            patterns: {
+              youtube: {
+                index: 'youtube.com/',
+          
+                id: 'v=', 
+          
+                src: 'https://www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+              },
+              vimeo: {
+                index: 'vimeo.com/',
+                id: '/',
+                src: '//player.vimeo.com/video/%id%?autoplay=1'
+              },
+              gmaps: {
+                index: '//maps.google.',
+                src: '%id%&output=embed'
+              }
+          
+              
+          
+            },
+          
+            srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+          }
+    })
+    //end playhighlight
     // popup Gallery End
 
     var toTop = document.querySelector(".to-top")
